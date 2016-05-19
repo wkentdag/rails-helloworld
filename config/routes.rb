979d734130fb90058a046bd8d2f115685d9ab072
Will Kent-Daggett <wkentdaggett@getty.edu>
2016-05-19 16:58:08 -0700

    insert some vars

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
+  root 'static_pages#home'
   get 'static_pages/help'
+  get 'static_pages/about'
 
   resources :posts
   resources :users
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  # root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
